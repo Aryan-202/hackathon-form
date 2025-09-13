f9e6725e5ac10599a1193e33fdef4bfef34e055b
Aryan Vishwakarma <aryanvishwakarma275@gmail.com>
2025-09-14 04:27:29 +0530

    added env

--- client/src/api/adminApi.js
+++ client/src/api/adminApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 // Configure axios to send cookies with requests
 axios.defaults.withCredentials = true;
--- client/src/api/teamApi.js
+++ client/src/api/teamApi.js
@@ -1,7 +1,7 @@
 // client/src/api/teamApi.js
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 // Set up axios defaults
 axios.defaults.withCredentials = true;

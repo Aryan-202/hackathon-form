45e63e66f874ebfa478425712ecdafc7f7dfab1e
Aryan Vishwakarma <aryanvishwakarma275@gmail.com>
2025-09-14 15:07:54 +0530

    changes

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:3000', {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useUnifiedTopology: false,
     });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
